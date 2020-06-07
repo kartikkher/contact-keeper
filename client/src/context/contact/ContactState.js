7258c05bad769f2572234a9aff2594d921a8fc5d
Kartik Kher <kartikkher777@gmail.com>
2020-06-07 19:46:42 +0100

    Frontend WIP: Set & Clear Curr contact / Contact filter Form & state implemented

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,8 @@ const ContactState = (props) => {
         type: 'professional',
       },
     ],
+    current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -60,21 +62,54 @@ const ContactState = (props) => {
   };
 
   // Set Current Contact
-
+  const setCurrent = (contact) => {
+    dispatch({
+      type: SET_CURRENT,
+      payload: contact,
+    });
+  };
   // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({
+      type: CLEAR_CURRENT,
+    });
+  };
 
   // Update Contact
+  const updateContact = (contact) => {
+    dispatch({
+      type: UPDTE_CONTACT,
+      payload: contact,
+    });
+  };
 
   // Filter Contacts
-
+  const filterContacts = (text) => {
+    dispatch({
+      type: FILTER_CONTACTS,
+      payload: text,
+    });
+  };
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({
+      type: CLEAR_FILTER,
+    });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
       }}>
       {props.children}
     </ContactContext.Provider>
